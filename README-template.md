b14323cb1987bf836274f57d24d2cab7354ca817
oliwiakramm <oliwiakramm@gmail.com>
2023-03-28 17:26:34 +0200

    initial commit

---
+++ README-template.md
@@ -0,0 +1,36 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](/images/screenShot.png)
+
+### Links
+
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Flexbox
+- Mobile-first workflow
+- Sass/SCSS
+
+## Author
+
+- Frontend Mentor - [@Hoshiko](https://www.frontendmentor.io/profile/oliwiakramm)
+
+## Acknowledgments
+
+I could get a better grasp of this project thanks to @elaineleung solution.
---
+++ script.js
@@ -0,0 +1,109 @@
+const form = document.querySelector("form");
+const billInp = document.getElementById("bill");
+const peopleInp = document.getElementById("people");
+const peopleEl = document.querySelector(".people__num");
+const errMsg = document.querySelector(".error__message");
+const tipPercInps = document.querySelectorAll("input[type='radio']");
+const customInp = document.getElementById("custom");
+const tipAmountEl = document.getElementById("tipAmount");
+const totalAmountEl = document.getElementById("totalAmount");
+const resetBtn = document.querySelector(".resetBtn");
+
+let bill, tip, people;
+const tipArray = [...tipPercInps];
+
+form.addEventListener("input", function (e) {
+  if (+billInp.value < 0) {
+    bill = null;
+  } else {
+    bill = +billInp.value;
+  }
+
+  if (e.target.id === "custom") {
+    customInput(+e.target.value);
+  }
+
+  if (e.target.id == form.tip.value) tipBtns();
+
+  if (e.target.id === "people") {
+    peopleVal(+e.target.value);
+  }
+  if (!peopleEl.classList.contains("error") && bill && tip && people) {
+    calculateTip();
+  } else {
+    clearResults();
+  }
+});
+
+function peopleVal(value) {
+  switch (true) {
+    case Number.isInteger(value) && value != 0 && value > 0:
+      if (peopleEl.classList.contains("error")) {
+        peopleEl.classList.remove("error");
+      }
+      people = value;
+      break;
+    case value === 0:
+      errMsg.textContent = "Can't be zero";
+      peopleEl.classList.add("error");
+      break;
+    case value < 0 || !Number.isInteger(value):
+      errMsg.textContent = "Wrong format";
+      peopleEl.classList.add("error");
+      break;
+    default:
+      null;
+  }
+}
+
+function customInput(value) {
+  tipArray.forEach((tipEl) => {
+    tipEl.checked = false;
+  });
+  if (+value < 0) {
+    tip = null;
+  } else {
+    tip = +value;
+  }
+}
+
+function tipBtns() {
+  customInp.value = "";
+  tipArray.forEach((tipEl) => {
+    if (tipEl.checked) {
+      tip = tipEl.value;
+    }
+  });
+}
+
+function calculateTip() {
+  resetBtn.removeAttribute("disabled");
+  let tipAmount = (bill * (tip / 100)) / people;
+  tipAmountEl.textContent = `$${tipAmount.toFixed(2)}`;
+
+  let totalAmount = bill / people + tipAmount;
+  totalAmountEl.textContent = `$${totalAmount.toFixed(2)}`;
+
+  if (tipAmountEl.textContent.length > 8) {
+    tipAmountEl.style.overflowX = "auto";
+    totalAmountEl.style.overflow = "auto";
+  }
+}
+
+function clearResults() {
+  tipAmountEl.textContent = `$0.00`;
+  totalAmountEl.textContent = `$0.00`;
+  resetBtn.setAttribute("disabled", true);
+}
+
+resetBtn.addEventListener("click", reset);
+
+window.addEventListener("load", reset);
+
+function reset() {
+  form.reset();
+  clearResults();
+  bill = null;
+  tip = null;
+  people = null;
+}
